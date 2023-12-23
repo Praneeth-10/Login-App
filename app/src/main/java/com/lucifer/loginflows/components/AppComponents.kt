8ffecba1b6b27dc75abd1e9a5e7a483a47fb8e82
Praneeth-10 <sp.parasa@gmail.com>
2023-12-22 20:53:07 -0600

    added heading components.

--- app/src/main/java/com/lucifer/loginflows/components/AppComponents.kt
+++ app/src/main/java/com/lucifer/loginflows/components/AppComponents.kt
@@ -2,24 +2,44 @@ package com.lucifer.loginflows.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.lucifer.loginflows.R
 
 @Composable
-fun NormalTextComponent(value: String) {
+fun NormalTextComponent(
+    value: String,
+    modifier: Modifier = Modifier,
+    style: TextStyle = TextStyle(
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Normal,
+        fontStyle = FontStyle.Normal,
+    ),
+) {
     Text(
         text = value,
-        modifier = Modifier.fillMaxWidth().heightIn(80.dp),
-        style = TextStyle(
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Normal,
-            fontStyle = FontStyle.Normal
-        )
+        modifier = modifier,
+        style = style,
+        color = colorResource(id = R.color.colorText),
+        textAlign = TextAlign.Center
     )
+}
+
+@Composable
+fun MyTextField() {
+
+    val textValue by remember{ mutableStateOf("") }
+//    OutlinedTextField(value = textValue, onValueChange = )
 }
--- app/src/main/java/com/lucifer/loginflows/screens/SignUpScreen.kt
+++ app/src/main/java/com/lucifer/loginflows/screens/SignUpScreen.kt
@@ -1,22 +1,59 @@
 package com.lucifer.loginflows.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.lucifer.loginflows.R
 import com.lucifer.loginflows.components.NormalTextComponent
 
 @Composable
 fun SignUpScreen() {
     Surface(
-        color = Color.White,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        color = Color.White
     ) {
-        NormalTextComponent(value = stringResource(id = R.string.hello))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp),
+            verticalArrangement = Arrangement.Top
+        ) {
+            NormalTextComponent(
+                value = stringResource(id = R.string.hello),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(18.dp)
+            )
+            NormalTextComponent(
+                value = stringResource(id = R.string.create_account),
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    fontStyle = FontStyle.Normal
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(40.dp)
+            )
+        }
     }
 }
 
