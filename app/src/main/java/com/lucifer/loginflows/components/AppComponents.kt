4dbc0151d52df1d562a8f1afaa747dd3b26d61c1
Praneeth-10 <sp.parasa@gmail.com>
2023-12-26 23:59:17 -0600

    Added the Registration form elements.

--- app/src/main/java/com/lucifer/loginflows/components/AppComponents.kt
+++ app/src/main/java/com/lucifer/loginflows/components/AppComponents.kt
@@ -1,32 +1,48 @@
 package com.lucifer.loginflows.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.VisibilityOff
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.lucifer.loginflows.R
+import com.lucifer.loginflows.ui.theme.Purple40
 
 @Composable
 fun NormalTextComponent(
@@ -48,7 +64,7 @@ fun NormalTextComponent(
 }
 
 @Composable
-fun MyTextField(labelValue: String) {
+fun MyTextField(labelValue: String, imageVector: ImageVector) {
 
     val textValue = remember {
         mutableStateOf("")
@@ -69,14 +85,108 @@ fun MyTextField(labelValue: String) {
             textValue.value = it
         },
         leadingIcon = {
-            Image(imageVector = Icons.Outlined.Person,
-                contentDescription = "person")
+            Image(
+                imageVector = imageVector,
+                contentDescription = ""
+            )
         }
     )
 }
 
+
+@Composable
+fun MyPasswordField(labelValue: String, imageVector: ImageVector) {
+
+    val passwordValue = remember {
+        mutableStateOf("")
+    }
+
+    val passwordVisible = remember {
+        mutableStateOf(false)
+    }
+
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        label = { Text(text = labelValue) },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = colorResource(id = R.color.colorPrimary),
+            focusedLabelColor = colorResource(id = R.color.colorPrimary),
+            cursorColor = colorResource(id = R.color.colorPrimary),
+            unfocusedContainerColor = Color(0xFFF7F8F8)
+        ),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        value = passwordValue.value,
+        onValueChange = {
+            passwordValue.value = it
+        },
+        leadingIcon = {
+            Image(
+                imageVector = imageVector,
+                contentDescription = ""
+            )
+        },
+        trailingIcon = {
+            IconButton(onClick = { passwordVisible.value = !passwordVisible.value }) {
+                Icon(
+                    imageVector = if (passwordVisible.value) {
+                        Icons.Filled.Visibility
+                    } else {
+                        Icons.Outlined.VisibilityOff
+                    },
+                    contentDescription = if (passwordVisible.value) {
+                        stringResource(R.string.show_password)
+                    } else {
+                        stringResource(R.string.hide_password)
+                    }
+                )
+            }
+        },
+        visualTransformation =
+            if (passwordVisible.value)  VisualTransformation.None
+            else PasswordVisualTransformation()
+    )
+}
+
+@Composable
+fun CheckBoxComponent(value: String) {
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        val checkBoxValue = remember {
+            mutableStateOf(false)
+        }
+
+        Checkbox(checked = checkBoxValue.value, onCheckedChange = {
+            checkBoxValue.value = !checkBoxValue.value
+        })
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+    }
+}
+
+@Composable
+fun ClickableTextComponent(value: String) {
+
+    val initialText = stringResource(id = R.string.by_continuing_you_accept)
+    val privacyPolicyText = stringResource(id = R.string.privacy_policy)
+    val andText = stringResource(id = R.string.and)
+    val termsText = stringResource(id = R.string.terms_of_use)
+
+    val annotatedString = buildAnnotatedString {
+        this.append(initialText)
+//        this.withStyle(style = SpanStyle(color = Purple40))
+    }
+}
+
+
 @Preview
 @Composable
 fun PrevMyText() {
-    MyTextField(labelValue = "First Name")
+//    MyTextField(labelValue = "First Name", imageVector = Icons.Outlined.Person)
+//    MyPasswordField(labelValue = stringResource(id = R.string.password), imageVector = Icons.Outlined.Lock )
 }
--- app/src/main/java/com/lucifer/loginflows/screens/SignUpScreen.kt
+++ app/src/main/java/com/lucifer/loginflows/screens/SignUpScreen.kt
@@ -3,13 +3,18 @@ package com.lucifer.loginflows.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -20,6 +25,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.lucifer.loginflows.R
+import com.lucifer.loginflows.components.MyPasswordField
 import com.lucifer.loginflows.components.MyTextField
 import com.lucifer.loginflows.components.NormalTextComponent
 
@@ -55,7 +61,26 @@ fun SignUpScreen() {
                     .heightIn(40.dp)
             )
 
-            MyTextField(labelValue = stringResource(id = R.string.firstName))
+            Spacer(modifier = Modifier.height(40.dp))
+            MyTextField(
+                labelValue = stringResource(id = R.string.first_name),
+                imageVector = Icons.Outlined.Person
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+            MyTextField(labelValue = stringResource(id = R.string.last_name), imageVector = Icons.Outlined.Person)
+
+            Spacer(modifier = Modifier.height(20.dp))
+            MyTextField(
+                labelValue = stringResource(id = R.string.email),
+                imageVector = Icons.Outlined.Email
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+            MyPasswordField(
+                labelValue = stringResource(id = R.string.password),
+                imageVector = Icons.Outlined.Lock
+            )
         }
     }
 }
